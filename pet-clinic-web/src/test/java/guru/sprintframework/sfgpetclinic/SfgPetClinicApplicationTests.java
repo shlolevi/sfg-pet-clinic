5c3982311dd71638d9c3361ab0df1b8a960be9c3
shlolevi <>
2023-02-11 14:23:42 +0200

    refactor to multimodules maven project. closes #1

--- pet-clinic-web/src/test/java/guru/sprintframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/sprintframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,14 +1,9 @@
 package guru.sprintframework.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-
-	}
 
 }
