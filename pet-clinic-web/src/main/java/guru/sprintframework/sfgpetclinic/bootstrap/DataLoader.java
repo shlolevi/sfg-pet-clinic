378fcbdb168dcc4e671ab1a9002be2fad425c579
shlolevi <s.levi@proxymity.io>
2023-05-18 14:30:41 +0300

    Bootstrap data closes #16

--- pet-clinic-web/src/main/java/guru/sprintframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/sprintframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,4 +1,4 @@
-package bootstrap;
+package guru.sprintframework.sfgpetclinic.bootstrap;
 
 import guru.sprintframework.sfgpetclinic.model.Owner;
 import guru.sprintframework.sfgpetclinic.model.Vet;
@@ -14,7 +14,6 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-
     public DataLoader(OwnerService ownerService, VetService vetService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
@@ -23,30 +22,26 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
-        owner2.setLastName("Gleanna");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
         System.out.println("Loaded owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
--- pet-clinic-web/src/main/java/guru/sprintframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/sprintframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package guru.sprintframework.sfgpetclinic.controllers;
 
+import guru.sprintframework.sfgpetclinic.model.guru.sprintframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller()
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String ListOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
