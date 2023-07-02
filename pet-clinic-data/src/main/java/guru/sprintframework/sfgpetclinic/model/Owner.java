34440df3c831a19321323e7eb2a27cc9e735b9c2
shlolevi <s.levi@proxymity.io>
2023-07-02 15:43:24 +0300

    Create Pet Type, Pet and Visit Entities Closes #25

--- pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,15 @@
 package guru.sprintframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
