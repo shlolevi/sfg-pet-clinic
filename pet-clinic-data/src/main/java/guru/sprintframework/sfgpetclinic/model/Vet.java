d09c24c7ca4368bbd392b9704d3ee011534fdb8a
shlolevi <s.levi@proxymity.io>
2023-07-02 15:47:46 +0300

    Create Vet speciality entity and associate to Vet. Closes #26

--- pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package guru.sprintframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Speciality> specialitySet;
+
+    public Set<Speciality> getSpecialitySet() {
+        return specialitySet;
+    }
+
+    public void setSpecialitySet(Set<Speciality> specialitySet) {
+        this.specialitySet = specialitySet;
+    }
 }
