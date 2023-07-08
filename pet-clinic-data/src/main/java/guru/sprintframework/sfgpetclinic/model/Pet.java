ff8b9ef20be3cd7f954f51430254ddc241e2a5e4
shlolevi <s.levi@proxymity.io>
2023-07-08 20:11:53 +0300

    Implement Base Entity (JPA). Closes #35

--- pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/model/Pet.java
@@ -1,12 +1,19 @@
 package guru.sprintframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
-
+    @Column(name = "name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birthday")
     private LocalDate birthDay;
 
     public String getName() {
