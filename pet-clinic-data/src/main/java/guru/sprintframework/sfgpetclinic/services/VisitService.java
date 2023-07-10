4fb1f318f42eefd41ff56e33f2f25868126ab495
shlolevi <s.levi@proxymity.io>
2023-07-10 15:22:29 +0300

    Write tests for Owner spring data JPA service. Closes #46

---
+++ pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.sprintframework.sfgpetclinic.services;
+
+import guru.sprintframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/sprintframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package guru.sprintframework.sfgpetclinic.services.map;
+
+import guru.sprintframework.sfgpetclinic.model.Visit;
+import guru.sprintframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
